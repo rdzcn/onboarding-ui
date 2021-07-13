104909f4c6bdcedfa51c26bfcc261fc84252aea5
Arda Özcan <arda.oezcan@aroundhome.de>
2021-07-14 00:44:43 +0200

    install additional packages

---
+++ .eslintrc.js
@@ -0,0 +1,97 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable unicorn/prefer-module */
+
+const restrictedGlobals = require("confusing-browser-globals");
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "airbnb-typescript",
+    "plugin:import/typescript",
+    "plugin:unicorn/recommended",
+    "plugin:lodash/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+    project: ["./tsconfig.eslint.json"],
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": ["error"],
+    "no-restricted-globals": ["error", ...restrictedGlobals],
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+    "unicorn/filename-case": [
+      "error",
+      {
+        cases: {
+          camelCase: true,
+          pascalCase: true,
+        },
+      },
+    ],
+    "no-restricted-imports": "error",
+
+    // Turn off unwanted rules from presets:
+    "consistent-return": "off",
+    "react/prop-types": "off",
+    "react/require-default-props": "off",
+    "unicorn/no-array-for-each": "off",
+    "unicorn/no-array-reduce": "off",
+    "unicorn/no-null": "off",
+    "unicorn/no-useless-undefined": "off",
+    "unicorn/prefer-includes": "off",
+    "unicorn/prefer-node-protocol": "off",
+    "unicorn/prefer-number-properties": "off",
+    "unicorn/prevent-abbreviations": "off",
+    "unicorn/prefer-query-selector": "off",
+  },
+  overrides: [
+    {
+      files: ["cypress/**"],
+      extends: ["plugin:cypress/recommended", "prettier"],
+    },
+    {
+      files: ["**/__tests__/**", "**/*.test.js"],
+      env: {
+        browser: true,
+        es2021: true,
+        node: true,
+        jest: true,
+      },
+      rules: {
+        "import/no-extraneous-dependencies": [
+          "error",
+          { devDependencies: true },
+        ],
+        "unicorn/no-array-callback-reference": "off",
+      },
+    },
+    {
+      files: ["*.styles.ts", "*.styles.js", "*.style.ts", "*.style.js"],
+      rules: {
+        "import/prefer-default-export": "off",
+      },
+    },
+  ],
+};
