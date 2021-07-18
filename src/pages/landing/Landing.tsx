2facc1c742e6305b540d60a7fe2493fd5b77a33a
Arda Özcan <arda.oezcan@aroundhome.de>
2021-07-18 14:56:40 +0200

    simple redirect for the danding page

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,11 +1,18 @@
-import React from "react";
-import { useLocation } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 import SelectLanguage from "../../components/selectLanguage/SelectLanguage";
 import SelectProfile from "../../components/selectProfile/SelectProfile";
 import { LandingContainer } from "./landing.styles";
 
 const Landing = () => {
   const { pathname } = useLocation();
+  const history = useHistory();
+
+  useEffect(() => {
+    if (pathname === "/") {
+      history.replace("/landing/language");
+    }
+  }, []);
 
   return (
     <LandingContainer>
